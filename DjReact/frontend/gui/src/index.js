fc7d6175afab1d05194f30102d955dd5d1cf20da
Salman Ahmed <ahmedsalman392@gmail.com>
2019-12-13 11:56:17 +0530

    added auth

--- DjReact/frontend/gui/src/index.js
+++ DjReact/frontend/gui/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, compose, applyMiddleware } from 'redux';
+import reducer from './store/reducers/authReducer';
+import thunk from 'react-redux';
+
+const composeEnhances = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(reducer, composeEnhances(
+    applyMiddleware(thunk)
+));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
